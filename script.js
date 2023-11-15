eba6186b55cc7bd9f7a969fcae3ec072efd43a15
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-15 10:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+   return ((year % 4 === 0 && year % 100 > 0) || year %400 == 0) ? 366 : 365;
+}
+
+// take input
+let input = prompt('Enter a year:');
+
+alert(daysOfAYear(input));
